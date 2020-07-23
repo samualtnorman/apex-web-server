63b37dd6b9ae6a7b932a35789781de7f99b890dd
Samual Norman <samualtnorman@gmail.com>
2020-07-24 00:31:11 +0100

    cleaned some stuff up

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Server as HTTPServer } from "http"
 import { Server as HTTPSServer } from "https"
-import { lookup } from "mime-types"
-import { resolve, dirname } from "path"
-import { parse } from "url"
-import { promises as fs, createReadStream } from "fs"
-import { load } from "yamljs"
+import { lookup as lookupType } from "mime-types"
+import { resolve as resolvePath, dirname } from "path"
+import { parse as parseURL } from "url"
+import { promises, createReadStream } from "fs"
+import { load as loadYaml } from "yamljs"
+import { formatWithOptions } from "util"
 
 interface LooseObject<T = any> {
 	[key: string]: T
@@ -13,9 +14,13 @@ interface LooseObject<T = any> {
 interface Config {
 	redirects: LooseObject<string>
 	symlinks: LooseObject<string>
+	httpPort: number
+	httpsPort: number
 }
 
-Promise.all([ fs.readFile(resolve(__dirname, "privkey.pem")), fs.readFile(resolve(__dirname, "fullchain.pem") )]).then(([ key, cert ]) => {
+const { readFile, stat } = promises
+
+Promise.all([ readFile(resolvePath(__dirname, "privkey.pem")), readFile(resolvePath(__dirname, "fullchain.pem") )]).then(([ key, cert ]) => {
 	new HTTPServer((req, res) => {
 		if (req.headers.host) {
 			const href = `https://${req.headers.host}${req.url || ""}`
@@ -28,11 +33,9 @@ Promise.all([ fs.readFile(resolve(__dirname, "privkey.pem")), fs.readFile(resolv
 	}).listen(80)
 
 	new HTTPSServer({ key, cert }, (req, res) => {
-		const { redirects, symlinks }: Config = load("config.yml")
-
 		if (req.headers.host) {
-			if (redirects[req.headers.host]) {
-				const href = redirects[req.headers.host]
+			if (config && config.redirects && typeof config.redirects == "object" && config.redirects[req.headers.host]) {
+				const href = config.redirects[req.headers.host]
 
 				console.log(301, req.headers.host, "->", href)
 				res.writeHead(301, { Location: href })
@@ -40,106 +43,121 @@ Promise.all([ fs.readFile(resolve(__dirname, "privkey.pem")), fs.readFile(resolv
 			} else {
 				let dir: string
 
-				if (symlinks[req.headers.host])
-					dir = symlinks[req.headers.host]
+				if (config && config.symlinks && typeof config.symlinks == "object" && config.symlinks[req.headers.host])
+					dir = config.symlinks[req.headers.host]
 				else {
 					dir = req.headers.host
-					dir += parse(req.url?.replace(/\.\./g, "") || "/").pathname || "/"
+					dir += parseURL(req.url?.replace(/\.\./g, "") || "/").pathname || "/"
 				}
 
 				if (dir.slice(-1) == "/")
 					dir += "index.html"
 				
 				const range = req.headers.range
-				const path = resolve(__dirname, "web", dir)
+				const path = resolvePath(__dirname, "web", dir)
 
-				if (range) {
-					fs.stat(path).then(({ size }) => {
-						const [ startStr, endStr ] = range.replace(/bytes=/, "").split("-");
-						let start = parseInt(startStr)
-						let end = parseInt(endStr)
+				stat(path).then(stats => {
+					if (stats.isFile()) {
+						const options: Parameters<typeof createReadStream>[1] = {}
 
-						end = isNaN(end) ? size - 1 : end
-						start = isNaN(start) ? size - end : start
-	
-						if (start >= size || end >= size) {
-							console.log(416, dir, `${start}-${end}/${size}`)
-							res.writeHead(416, { "Content-Range": `bytes */${size}` })
-							res.end()
-						} else {
-							console.log(206, dir, `${start}-${end}/${size}`)
+						res.setHeader("Content-Type", lookupType(dir) || "text/plain")
+						res.setHeader("Content-Location", `https://${dir}`)
+						
+						if (range) {
+							const [ startStr, endStr ] = range.replace(/bytes=/, "").split("-")
+
+							let start = parseInt(startStr)
+							let end = parseInt(endStr)
+
+							end = isNaN(end) ? stats.size - 1 : end
+							start = isNaN(start) ? stats.size - end : start
+
+							options.start = start
+							options.end = end
+
+							console.log(206, dir, `${start}-${end}/${stats.size}`)
 
 							res.writeHead(206, {
-								"Content-Range": `bytes ${start}-${end}/${size}`,
+								"Content-Range": `bytes ${start}-${end}/${stats.size}`,
 								"Accept-Ranges": "bytes",
 								"Content-Length": end - start + 1,
-								"Content-Type": lookup(dir) || "text/plain"
 							})
-		
-							createReadStream(path, { start, end }).pipe(res)
+						} else {
+							console.log(200, dir)
+							res.writeHead(200, { "Content-Length": stats.size })
 						}
-					}).catch(err => console.error(err))
-				} else {
-					fs.readFile(path).then(value => {
-						console.log(200, dir)
-						res.writeHead(200, { "Content-Type": lookup(dir) || "text/plain" })
-						res.end(value)
-					}, (reason: NodeJS.ErrnoException | null) => {
-						let href: string
 
-						switch (reason?.errno) {
-							case -2:
-							case -21:
-							case -4058:
-								console.log(404, dir)
+						createReadStream(path, options).pipe(res)
+					} else {
+						const href = `https://${dir}/`
 
-								fs.readFile(resolve(__dirname,  "web/_status/404.html"))
-									.then(
-										value => {
-											res.writeHead(404, { "Content-Type": "text/html" })
-											res.end(value)
-										},
-										() => {
-											res.writeHead(404, { "Content-Type": "text/plain" })
-											res.end("404 not found")
-										}
-									)
-								break
-							case -20:
-								href = `https://${dirname(dir)}`
+						console.log(301, dir, "->", href)
 
-								console.log(301, dir, "->", href)
+						res.writeHead(301, { Location: href })
+						res.end(`302 moved permanently\n${href}`)
+					}
+				}, (reason: NodeJS.ErrnoException | null) => {
+					let href: string
 
-								res.writeHead(301, { Location: href })
-								res.end(`302 moved permanently\n${href}`)
-								break
-							case -4068:
-								href = `https://${dir}/`
+					console.log(JSON.stringify(reason))
 
-								console.log(301, dir, "->", href)
+					switch (reason?.errno) {
+						case -2:
+						case -21:
+						case -4058:
+							console.log(404, dir)
 
-								res.writeHead(301, { Location: href })
-								res.end(`302 moved permanently\n${href}`)
-								break
-							default:
-								console.log(500, reason)
+							readFile(resolvePath(__dirname,  "meta/404.html"))
+								.then(
+									value => {
+										res.writeHead(404, { "Content-Type": "text/html" })
+										res.end(value)
+									},
+									() => {
+										res.writeHead(404, { "Content-Type": "text/plain" })
+										res.end("404 not found")
+									}
+								)
 
-								fs.readFile(resolve(__dirname,  "web/_status/500.html"))
-									.then(
-										value => {
-											res.writeHead(500, { "Content-Type": "text/html" })
-											res.end(value)
-										},
-										() => {
-											res.writeHead(500, { "Content-Type": "text/plain" })
-											res.end("500 internal server error")
-										}
-									)
-						}
-					})
-				}
+							break
+						case -20:
+							href = `https://${dirname(dir)}`
+
+							res.writeHead(301, { Location: href })
+							res.end(`301 moved permanently\n${href}`)
+							break
+						case -4068:
+							href = `https://${dir}/`
+
+							console.log(301, dir, "->", href)
+
+							res.writeHead(301, { Location: href })
+							res.end(`302 moved permanently\n${href}`)
+							break
+						default:
+							console.log(500, reason)
+
+							readFile(resolvePath(__dirname,  "web/_status/500.html"))
+								.then(
+									value => {
+										res.writeHead(500, { "Content-Type": "text/html" })
+										res.end(value)
+									},
+									() => {
+										res.writeHead(500, { "Content-Type": "text/plain" })
+										res.end("500 internal server error")
+									}
+								)
+					}
+				})
 			}
 		} else
 			res.end()
 	}).listen(443)
 })
+
+let config = loadYaml("config.yml")
+
+setTimeout(() => {
+	config = loadYaml("config.yml")
+}, 10000)
